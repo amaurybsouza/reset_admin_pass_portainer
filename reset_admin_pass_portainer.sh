bc8a68935d79271800a605fc6f3891ba849adfd0
amaurybsouza <amaurybsouza@gmail.com>
2024-04-21 21:43:05 -0300

    teste

--- reset_admin_pass_portainer.sh
+++ reset_admin_pass_portainer.sh
@@ -39,10 +39,10 @@ docker container stop 233
 docker pull portainer/helper-reset-password
 docker run --rm -v portainer_data:/data portainer/helper-reset-password
 
-# You will a similar output like below
-2024/04/21 07:36:33 [WARN] Unable to retrieve user with ID 1, will try to create, err: object not found inside the database
-2024/04/21 07:36:33 Admin user admin-u0512b3f0v4dqk7o successfully created
-2024/04/21 07:36:33 Use the following password to login: Sr#]YL_6D0k8Pd{pA9^|}F32j5J4I=av
+# You will a similar output like below...
+#2024/04/21 07:36:33 [WARN] Unable to retrieve user with ID 1, will try to create, err: object not found inside the database
+#2024/04/21 07:36:33 Admin user admin-u0512b3f0v4dqk7o successfully created
+#2024/04/21 07:36:33 Use the following password to login: Sr#]YL_6D0k8Pd{pA9^|}F32j5J4I=av
 
 # Start the Docker Portainer container
 docker start "233"
